10a64fdc570585a0689e6a914a5af0ac65243f7a
chengqian <du.chengqian@myquant.cn>
2023-05-30 18:25:39 +0800

    fix

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -10,10 +10,14 @@
 // Write a function that calculates the price of an order of apples given
 // the quantity bought. No hints this time!
 
-// I AM NOT DONE
-
 // Put your function here!
-// fn calculate_price_of_apples {
+fn calculate_price_of_apples(count: i32) -> i32 {
+    if count > 40 {
+        return count * 1
+    } 
+    
+    return count*2
+}
 
 // Don't modify this function!
 #[test]
